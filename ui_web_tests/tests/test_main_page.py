6a72f4bfbcd549be427bf5ea81d407a4ca9be1d0
Siron22 <s0949528587@gmail.com>
2023-02-12 12:33:43 +0100

    rename project

--- ui_web_tests/tests/test_main_page.py
+++ ui_web_tests/tests/test_main_page.py
@@ -7,7 +7,7 @@ def test_main_page_is_open(driver, main_page):
     assert 'Hillel Qauto' in driver.title
 
 
-def test_main_page_elements_displayed(main_page):
+def test_main_page_elements_displayed(driver, main_page):
     assert main_page.logo_hillel_auto.is_displayed()
     assert main_page.button_home.is_displayed()
     assert main_page.button_about.is_displayed()
@@ -18,7 +18,7 @@ def test_main_page_elements_displayed(main_page):
     assert main_page.string1.is_displayed()
     assert main_page.string2.is_displayed()
 
-def test_main_page_video_displayed(main_page):
+def test_main_page_video_displayed(driver, main_page):
     """Визуально объект присутствует, а тест почему-то падает"""
     assert main_page.video_locator.is_displayed()
 
